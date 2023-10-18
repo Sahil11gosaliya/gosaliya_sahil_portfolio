560897a616295d8bf1e8fb54757540806437e7c0
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-10-18 13:52:36 -0400

    planning for masonry grid and dynamic pages

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-(function() {
-    // Selecting the elements
+(() => {
+    console.log("IIFE Fired");
+  
+    // Masonry Grid
+
+    // Hamburger Menu
     const hamMenu = document.querySelector('.ham');
     const hamIconOpen = document.querySelector('.menu-open');
     const hamIconClose = document.querySelector('.menu-close');
-
-    // Initially hide the ham menu
+  
     hamMenu.style.display = 'none';
-
-    // Event listener to toggle the visibility of the ham menu
+  
     hamIconOpen.addEventListener('click', function() {
-        hamMenu.style.display = 'block';
+      hamMenu.style.display = 'block';
     });
-
+  
     hamIconClose.addEventListener('click', function() {
-        hamMenu.style.display = 'none';
+      hamMenu.style.display = 'none';
     });
-})();
+  })();
+  
+
+
+
+  //dynamic work pages
+
+  //magic pointer
+
+  //rotating crystal icon
+
+  //gif
+
+  

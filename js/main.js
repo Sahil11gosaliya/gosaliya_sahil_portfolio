d94f44288af9aea0d8a73bf25bdc310b64920870
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-12-10 14:06:30 -0500

    updated and validated the code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 (() => {
   console.log("IIFE Fired");
 
-  // Masonry Grid
-
   // Hamburger Menu
   const hamMenu = document.querySelector('.ham');
   const hamIconOpen = document.querySelector('.menu-open');
@@ -17,21 +15,13 @@
   hamIconClose.addEventListener('click', function () {
     hamMenu.style.display = 'none';
   });
-})();
 
+  // dynamic work pages
+  // magic pointer
+  // rotating crystal icon
+  // gif
 
-
-
-//dynamic work pages
-
-//magic pointer
-
-//rotating crystal icon
-
-//gif
-
-(function () {
-  document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', () => {
     const player = new Plyr('#plyr-video', {
       controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
       tooltips: { controls: true, seek: true },
@@ -39,18 +29,12 @@
       iconUrl: 'https://cdn.plyr.io/3.6.2/plyr.svg',
     });
   });
-})();
 
-//video
-
-
-
-//navlink
-
-//wand
-// magic-reveal.js
-(function () {
-  document.addEventListener("DOMContentLoaded", function () {
+  // video
+  // navlink
+  // wand
+  // magic-reveal.js
+  document.addEventListener("DOMContentLoaded", () => {
     const sectionText = document.querySelector(".front .text");
 
     // SplitText
@@ -73,44 +57,71 @@
       },
     });
   });
-})();
 
+  // common.js
+  document.addEventListener("DOMContentLoaded", () => {
+    const wandElements = document.querySelectorAll(".wand");
 
-(function () {
-  document.addEventListener("DOMContentLoaded", function () {
-    const wand = document.querySelector(".wand");
+    wandElements.forEach((wand) => {
+      // GSAP Animation
+      const tl = gsap.timeline({ defaults: { ease: "power2.out" }, repeat: -1 });
 
-    // GSAP Animation
-    const tl = gsap.timeline({ defaults: { ease: "power2.out" }, repeat: -1 });
+      // Initial state
+      tl.set(wand, { opacity: 1, filter: "drop-shadow(10px 0 10px rgba(255, 255, 255, 0))" });
 
-    // Initial state
-    tl.set(wand, { opacity: 1, filter: "drop-shadow(10px 0 10px rgba(255, 255, 255, 0))" });
+      // Animation to make the drop shadow glow and fade
+      tl.to(wand, { filter: "drop-shadow(20px 0 20px rgba(255, 255, 255, 0.5))", duration: 0.5 })
+        .to(wand, { filter: "drop-shadow(10px 0 10px rgba(255, 255, 255, 0))", duration: 0.5 });
+    });
 
-    // Animation to make the drop shadow glow and fade
-    tl.to(wand, { filter: "drop-shadow(20px 0 20px rgba(255, 255, 255, 0.5))", duration: 0.5 })
-      .to(wand, { filter: "drop-shadow(10px 0 10px rgba(255, 255, 255, 0))", duration: 0.5 });
-  });
-})();
+    const introTextElements = document.querySelectorAll('.intro-text p');
 
-(function () {
-  document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("mousemove", function (e) {
-      const sparkle = document.createElement('div');
-      sparkle.className = 'sparkle';
-
-      const star = document.createElement('div');
-      star.className = 'star';
-      sparkle.appendChild(star);
-
-      sparkle.style.left = (e.pageX - 5) + 'px';
-      sparkle.style.top = (e.pageY - 5) + 'px';
-
-      document.body.appendChild(sparkle);
-
-      // Remove sparkles after animation completes
-      sparkle.addEventListener('animationend', function () {
-        document.body.removeChild(this);
+    introTextElements.forEach((element) => {
+      gsap.from(element, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: element,
+          start: 'top 80%',
+          end: 'bottom 20%',
+          toggleActions: 'play play none reverse',
+        },
       });
     });
+
+    // Toggle Read More
+    function toggleReadMore() {
+      const readMoreContent = document.querySelector('.read-more-content');
+      const readMoreToggle = document.querySelector('.read-more-toggle');
+
+      if (readMoreContent.style.display === 'none' || !readMoreContent.style.display) {
+        readMoreContent.style.display = 'inline';
+        readMoreToggle.textContent = 'Read Less';
+      } else {
+        readMoreContent.style.display = 'none';
+        readMoreToggle.textContent = 'Read More';
+      }
+    }
+
+    // Initially hide the extra content
+    document.querySelector('.read-more-content').style.display = 'none';
+
+    // Attach the event listener
+    document.querySelector('.read-more-toggle').addEventListener('click', toggleReadMore);
   });
 })();
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const formFields = document.querySelectorAll('.field');
+
+  gsap.from(formFields, {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    stagger: 0.2, // Adjust the stagger for a sequential reveal
+  });
+});
